5d1a1737650489c6a55b5defaf199b6eee91ff8e
RadchenkoVlada <ladon95@gmail.com>
2019-03-07 11:48:57 +0200

    I have added a comment to task3_1

--- task3_1.py
+++ task3_1.py
@@ -1,5 +1,16 @@
+"""
+Exercise 1:
+Rewrite your pay computation to give the employee 1.5 times the hourly rate for hours worked above 40 hours.
+
+Enter Hours: 45
+Enter Rate: 10
+Pay: 475.0
+"""
+
+
 def compute_pay(hours, rate):
     return 40 * rate + (hours - 40) * rate * 1.5 if hours > 40 else hours * rate
+
 
 if __name__ == '__main__':
     try:

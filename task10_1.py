609a26cefea8cf3849034e2f6ba4a7eb8ec0110d
RadchenkoVlada <ladon95@gmail.com>
2019-04-18 19:03:32 +0300

    The file has been edited.

--- task10_1.py
+++ task10_1.py
@@ -16,47 +16,24 @@ zqian@umich.edu 195
 def record_the_adresses(filename):
     with open(filename, "r") as file:
         dictionary = {}
-        count = 0
         for line in file:
             if line[:5] == "From ":
                 word_list = line.split()
                 email = word_list[1]
-                # s.find(), s.rfind(). Они возвращают индексы первого и последнего вхождения искомой подстроки.
-                #  Если же подстрока не найдена, то метод возвращает значение -1.
                 if email not in dictionary:
                     dictionary[email] = dictionary.get(email, 1)
                 else:
                     dictionary[email] = dictionary.get(email) + 1
         t = list()
         for key, val in dictionary.items():
-            # t.append((val, key))
             t.append((key, val))
 
         t.sort(reverse=True)
-        # если я что-либо print после сортировки, то it become this way
-        """
-        zqian@umich.edu 195
-        zach.thomas@txstate.edu 17
-        wagnermr@iupui.edu 44
-        tnguyen@iupui.edu 6
-        thoppaymallika@fhda.edu 1
-        stuart.freeman@et.gatech.edu 17
-        stephen.marquard@uct.ac.za 29
-        ssmail@indiana.edu 5
-        sgithens@caret.cam.ac.uk 43
-        rjlowe@iupui.edu 90
-        
-        """
-        # print("The list of tuples sorted by value: ", t)
+
         for key, val in t[:10]:
             print("The list of tuples sorted by value: ", key, val)
 
-        # for key, val in t:
-        #     print(val, key)
-
-        # странно попросили одно, хотят другое в output
         return t[0]
-
 
 
 if __name__ == '__main__':

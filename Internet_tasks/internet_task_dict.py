3bbb69bdd72acc39bba835295c5ebada617f0fb1
RadchenkoVlada <ladon95@gmail.com>
2019-04-19 17:18:26 +0300

    Tasks from internet have done.

---
+++ Internet_tasks/internet_task_dict.py
@@ -0,0 +1,146 @@
+"""
+This is the site from there i have taken information.
+https://server.179.ru/tasks/python/2014b1/17-sets.html
+
+K: Номер появления слова
+Во входном файле (вы можете читать данные из файла input.txt) записан текст. Словом считается последовательность
+непробельных символов идущих подряд, слова разделены одним или большим числом пробелов или символами конца строки.
+
+Для каждого слова из этого текста подсчитайте, сколько раз оно встречалось в этом тексте ранее.
+
+L: Словарь синонимов
+Вам дан словарь, состоящий из пар слов. Каждое слово является синонимом к парному ему слову. Все слова в словаре
+различны. Для одного данного слова определите его синоним.
+
+Программа получает на вход количество пар синонимов N. Далее следует N строк, каждая строка содержит ровно два
+слова-синонима. После этого следует одно слово.
+
+Программа должна вывести синоним к данному слову.
+
+M: Выборы в США
+Как известно, в США президент выбирается не прямым голосованием, а путем двухуровневого голосования. Сначала проводятся
+выборы в каждом штате и определяется победитель выборов в данном штате. Затем проводятся государственные выборы:
+на этих выборах каждый штат имеет определенное число голосов — число выборщиков от этого штата. На практике, все
+выборщики от штата голосуют в соответствии с результами голосования внутри штата, то есть на заключительной стадии
+выборов в голосовании участвуют штаты, имеющие различное число голосов.
+
+Вам известно за кого проголосовал каждый штат и сколько голосов было отдано данным штатом. Подведите итоги выборов: для
+каждого из участника голосования определите число отданных за него голосов.
+
+Каждая строка входного файла содержит фамилию кандидата, за которого отдают голоса выборщики этого штата, затем через
+пробел идет количество выборщиков, отдавших голоса за этого кандидата.
+
+Выведите фамилии всех кандидатов в лексикографическом порядке, затем, через пробел, количество отданных за них голосов.
+
+N: Самое частое слово
+Дан текст. Выведите слово, которое в этом тексте встречается чаще всего. Если таких слов несколько, выведите то,
+которое меньше в лексикографическом порядке.
+
+Test from softserve
+
+name: parse_number()
+Please provide full program code of function which returns the dict with following structure: {odd: number of odd digits
+in input value, even: number of even digits of input value} or false when wrong input value.
+num-input number
+NOTE: Assume that the "zero" digit also belongs to even numbers.
+"""
+import random
+
+def number_of_the_word(filename):
+    with open(filename, "r") as file:
+        dictionary = {}
+        # firstNlines = file.readlines()[0:100]
+        for line in file:
+            word_list = line.split()
+            for word in word_list:
+                if word not in dictionary:
+                    dictionary[word] = 0
+                else:
+                    dictionary[word] = dictionary.get(word) + 1
+                print(dictionary[word], end=' ')
+            # return dictionary
+
+
+def synonyms():
+    synonyms = dict(oat="porridge", hello='hi', bye='goodbye', list='array', popcorn='corn', lady='girl', dame='queen',
+                    mum='mother', sis='sister', dad='father')
+
+    count_syn = random.randint(1, 6)
+    print("Count of synonyms", count_syn)
+    for i in range(count_syn):
+        print(random.choice(list(synonyms.items())))
+    syn = "lady"  # input("Please, enter the word for which you want to find a synonym:")
+    for syn1, syn2 in synonyms.items():
+        if syn1 == syn:
+            print(syn2)
+            print("if")
+            break
+        elif syn2 == syn:
+            print(syn1)
+            print("elif")
+            break
+        print(f"{syn1} {syn2} after if \n")
+
+    print("End of the program")
+    return 0  # how many returns do i need in the program or at all
+
+
+def voting():
+    candidates = dict(McCain=0, Obama=0, Trump=0, Weld=0)
+    for i in range(7):
+        candidate = random.choice(list(candidates.keys()))
+        vote = random.randint(1, 11)
+        candidates[candidate] += vote
+        print(candidate, vote)
+    print("Presidential Election Results:")
+    list_keys = list(candidates.keys())  # i still haven't realized how the correct lexicographical order looks like
+    list_keys.sort()
+    for i in list_keys:
+        print(i, candidates[i])
+
+
+def most_frequent_word(filename):
+    with open(filename, "r") as file:
+        dictionary = {}
+        for line in file:
+            word_list = line.split()
+            for word in word_list:
+                if word not in dictionary:
+                    dictionary[word] = dictionary.get(word, 1)
+                else:
+                    dictionary[word] = dictionary.get(word) + 1
+        # print(sorted(dictionary.items()))
+        # list_values = list(dictionary.values())
+        max_elem = max(dictionary.values())
+        print(max_elem)
+        answer = [key for key, value in sorted(dictionary.items()) if value == max_elem]
+        return answer
+
+
+def parse_number():
+    dictionary = {"odd": 0, "even": 0}
+    try:
+        num = input("Please enter a number: ")
+        numbers = int(num)  # check if input is correct
+        list_ = list(str(numbers))
+        list_numbers = []
+        for el in list_:  # i have a problem with "0000" int() breaks everything
+             list_numbers.append(int(el))
+        for number in list_numbers:
+            if number % 2 == 0:
+                dictionary["even"] += 1
+            # elif number == 0:
+            #     dictionary["even"] += 1
+            else:
+                dictionary["odd"] += 1
+    except ValueError:
+            print("False input")
+            exit()
+    return dictionary
+
+
+if __name__ == '__main__':
+    # print(number_of_the_word("romeo.txt"))  # task number K
+    # print(synonyms())
+    # print(voting())
+    print(parse_number())
---
+++ Internet_tasks/internet_task_sets.py
@@ -0,0 +1,194 @@
+"""
+This is the site from there i have taken information.
+https://server.179.ru/tasks/python/2014b1/17-sets.html
+
+A: Количество различных чисел
+Дан список чисел, который могут содержать до 100000 чисел каждый. Определите, сколько в нем встречается различных чисел.
+
+B: Количество совпадающих
+Даны два списка чисел, которые могут содержать до 100000 чисел каждый. Посчитайте, сколько чисел содержится одновременно
+как в первом списке, так и во втором.
+
+С: Пересечение списков
+Даны два списка чисел, которые могут содержать до 10000 чисел каждый. Выведите все числа, которые входят как в первый,
+так и во второй список в порядке возрастания.
+
+D: Встречалось ли число раньше
+Во входной строке записана последовательность чисел через пробел. Для каждого числа выведите слово YES
+(в отдельной строке), если это число ранее встречалось в последовательности или NO, если не встречалось.
+
+E: Кубики
+Аня и Боря любят играть в разноцветные кубики, причем у каждого из них свой набор и в каждом наборе все кубики различны
+по цвету. Однажды дети заинтересовались, сколько существуют цветов таких, что кубики каждого цвета присутствуют в обоих
+наборах. Для этого они занумеровали все цвета случайными числами. На этом их энтузиазм иссяк, поэтому вам предлагается
+помочь им в оставшейся части.
+
+Номер любого цвета — это целое число в пределах от 0 до 109. В первой строке входного файла записаны числа N и M —
+количество кубиков у Ани и Бори соответственно. В следующих N строках заданы номера цветов кубиков Ани. В последних M 
+строках номера цветов кубиков Бори.
+
+Выведите сначала количество, а затем отсортированные по возрастанию номера цветов таких, что кубики каждого цвета есть в
+обоих наборах, затем количество и отсортированные по возрастанию номера остальных цветов у Ани, потом количество и 
+отсортированные по возрастанию номера остальных цветов у Бори.
+
+F: Количество слов в тексте
+Во входном файле (вы можете читать данные из файла input.txt) записан текст. Словом считается последовательность
+непробельных символов идущих подряд, слова разделены одним или большим числом пробелов или символами конца строки.
+Определите, сколько различных слов содержится в этом тексте.
+
+G: Угадай число
+Август и Беатриса играют в игру. Август загадал натуральное число от 1 до n. Беатриса пытается угадать это число, для
+этого она называет некоторые множества натуральных чисел. Август отвечает Беатрисе YES, если среди названных ей чисел
+есть задуманное или NO в противном случае. После нескольких заданныъх вопросов Беатриса запуталась в том, какие вопросы
+она задавала и какие ответы получила и просит вас помочь ей определить, какие числа мог задумать Август.
+
+Первая строка входных данных содержит число n — наибольшее число, которое мог загадать Август. Далее идут строки,
+содержащие вопросы Беатрисы. Каждая строка представляет собой набор чисел, разделенных пробелами. После каждой строки с
+вопросом идет ответ Августа: YES или NO.
+
+Наконец, последняя строка входных данных содержит одно слово HELP. Вы должны вывести (через пробел, в порядке
+возрастания) все числа, которые мог задумать Август.
+
+I: Полиглоты
+Каждый из N школьников некоторой школы знает Mi языков. Определите, какие языки знают все школьники и языки, которые
+знает хотя бы один из школьников.
+
+Первая строка входных данных содержит количество школьников N. Далее идет N чисел Mi, после каждого из чисел идет
+Mi строк, содержищих названия языков, которые знает i-й школьник. Длина названий языков не превышает 1000 символов,
+количество различных языков не более 1000. 1≤N≤1000, 1≤Mi≤500.
+
+"""
+import random
+from itertools import chain
+
+
+def number_of_different_numbers():
+    list_input = [random.randint(-5, 100) for _ in range(20)]
+    print(list_input)
+    unique_word_count = len(set(list_input))
+    return unique_word_count
+
+
+def number_of_matches():
+    input1 = set([random.randint(-5, 100) for _ in range(10)])
+    print(input1)
+    input2 = set([random.randint(-5, 100) for _ in range(10)])
+    print(input2)
+    count = len(input1.intersection(input2))
+    return count
+
+
+def crossing_lists():
+    input1 = set([random.randint(-5, 100) for _ in range(10)])
+    print(input1)
+    input2 = set([random.randint(-5, 100) for _ in range(10)])
+    print(input2)
+    crossing = list(input1.intersection(input2))
+    crossing.sort()
+    return crossing
+
+
+def did_the_number_occur_before():
+    input = [random.randint(-5, 100) for _ in range(10)]
+    print(input)
+    numbers_set = set()
+    for elem in input:
+        if elem in numbers_set:
+            print('YES')
+        else:
+            print('NO')
+            numbers_set.add(elem)
+
+
+def blocks():
+    A_input = set([random.randint(0, 11) for _ in range(10)])
+    print(A_input)
+    B_input = set([random.randint(0, 11) for _ in range(10)])
+    raw_crossing = A_input.intersection(B_input)
+    crossing = sorted(list(raw_crossing))
+    count = len(crossing)
+    print(B_input)
+    print("Input")
+    print("Anna", len(A_input), "Boris", len(B_input))
+    print("The colors of Anna's blocks:")
+    for elem in A_input:
+        print(elem)
+    print("The colors of Boris' blocks:")
+    for elem in B_input:
+        print(elem)
+    print("Output:")
+    print('intersections of blocks', count)
+    print(crossing)
+    print("count of only Anna's blocks:", len(A_input.difference(B_input)))
+    print(sorted(list(A_input-B_input)))
+    print("count of only Boris' blocks:", len(B_input.difference(A_input)))
+    print(sorted(list(B_input-A_input)))
+
+
+def different_words_text(name_file):
+    try:
+        with open(name_file, "r") as file:
+            unique_words = set(chain(*(line.split() for line in file if line)))
+    except IOError:
+        print('File {0}'.format(name_file), "cannot be opened")
+        return 0
+    else:
+        return len(unique_words)
+
+
+def guess_the_number():
+    input_number = random.randint(0, 10)
+    print("It is a secret! Hush! A hidden number:", input_number)
+    print("The max number in which the hidden number is:", 10)
+    input_set = set([random.randint(0, 10) for _ in range(10)])
+    print(" ".join(map(str, input_set)))
+    output_set = set()
+
+    for i in range(10):
+        input_set = set([random.randint(0, 10) for _ in range(10)])
+        if input_number in input_set:
+            print("Yes\n")
+            output_set.update(input_set)
+            print(" ".join(map(str, input_set)))
+        else:
+            print("No\n")
+            output_set -= input_set
+            print(" ".join(map(str, input_set)))
+    print("Help!")
+    truthful_output = list(output_set)
+    sorted_truthful_output = sorted(truthful_output)
+    return " ".join(map(str, sorted_truthful_output))
+    # я хочу удалить последнюю выведеннуюс троку, т.к. на ответ она не влияет, но как?
+    #  и может алгоритм еще можно улучшить?
+# в большинстве случаев программа работает хорошо, особенно при увеличении range, но иногда очень большой ответ, если
+# все Yes
+
+
+def polyglots():
+    languages = {"Russian", "English", "Japanese", "German", "French", "Finnish"}
+    count_schoolkid = random.randint(1, 10)
+    print("Count of schoolkid", count_schoolkid)
+    list_of_set = []
+    for schoolkid in range(count_schoolkid):
+        count_languages = random.randint(1, 6)
+        schoolkid_lang = set(random.sample(languages, count_languages))
+        print("Languages ​​that the child knows", count_languages, ":", " ".join(map(str, schoolkid_lang)))
+        list_of_set.append(schoolkid_lang)
+    lang_know_all = set.intersection(*list_of_set)
+    print("Languages ​​spoken by all students:", lang_know_all)
+    lang_know_one = set.union(*list_of_set)
+    print("Languages ​​that at least one of the students knows:", lang_know_one)
+
+
+
+if __name__ == '__main__':
+    # print("A: Numbers of different values in the list",number_of_different_numbers())
+    # print("B: There are count of numbers at the same time in the first list and in the second:", number_of_matches())
+    # print("C: Displays all numbers included in both the first and second list in ascending order: ", crossing_lists())
+    # print("D:", did_the_number_occur_before())
+    # print("E:", blocks())
+    # words = different_words_text("romeo.txt")
+    # if words is not None:
+    #     print("F: Different words in the text -", words)
+    print("G: Output", guess_the_number())
+    # print("I:", polyglots())

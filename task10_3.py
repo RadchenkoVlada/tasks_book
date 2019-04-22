4f72d0468978ec8511b84cc5f585e201d30327b0
RadchenkoVlada <ladon95@gmail.com>
2019-04-22 17:08:54 +0300

    Сomments have been changed.

--- task10_3.py
+++ task10_3.py
@@ -33,29 +33,3 @@ def letter_counter(filename):
 if __name__ == '__main__':
     # filename = input("Enter a file name: ")
     print(letter_counter("english.txt"))
-
-"""
-import string
-fname = raw_input('Enter file name: ')
-try:
-	fhandle = open(fname)
-except:
-	print 'File cannot be opened:', fname
-	exit()
-letters = dict()
-for line in fhandle:
-	line = line.translate(None, string.punctuation)
-	line = line.translate(None, string.digits)
-	line = line.lower()
-	line = line.split()
-	for t in line:
-		word = list(t)
-		for letter in word:
-			letters[letter] = letters.get(letter,0) + 1
-letterlist = []
-for letter, count in letters.items():
-	letterlist.append( (count, letter) )
-letterlist.sort(reverse=True)
-for count, letter in letterlist:
-	print letter, count
-"""
